eeb689c69463e402edcfdc2d6bd1ba5642e0764e
honghanmyb <han.hong@utoronto.ca>
2018-11-29 23:22:05 -0500

    LeeCode 128
    Find longest consecutive sequence length in an unsorted array, use hashset

---
+++ src/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+	public int longestConsecutive(int[] nums) {
+		if(nums.length == 0) return 0;
+		int max = 1;
+		HashSet<Integer> numSet = new HashSet<>();
+		for(int i = 0; i < nums.length; i++) {
+			numSet.add(nums[i]);
+		}
+		for(int i = 0; i < nums.length; i++) {
+			if(numSet.contains(nums[i])) {
+				currentLongest = 0;
+				findConsecutive(nums[i], numSet);
+				if(max < currentLongest) max = currentLongest;
+			}
+		}
+		
+		return max;
+	}
+	
+	private void findConsecutive(int number, HashSet<Integer> numSet) {
+		if(!numSet.contains(number)) return;
+		numSet.remove(number);
+		currentLongest++;
+		findConsecutive(number - 1, numSet);
+		findConsecutive(number + 1, numSet);
+	}
+	
+	private int currentLongest = 1;
+}
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		SearchInsertPosition test = new SearchInsertPosition();
-		int[] nums = {1,3,5,6};
-		System.out.println(test.searchInsert(nums, 2));
+		LongestConsecutiveSequence test = new LongestConsecutiveSequence();
+		
+		int[] nums = {100, 4, 200, 1, 3, 2,5,8,9,6,7,50,49,48,47};
+		System.out.println(test.longestConsecutive(nums));
 	}
 }
