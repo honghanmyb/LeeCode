00a0436a3c6151c68d577b50de363937fb198221
honghanmyb <han.hong@mail.utoronto.ca>
2019-01-14 00:16:43 -0500

    Partition Palindrome
    Implement it recursively

--- src/PalindromePartitioning.java
+++ src/PalindromePartitioning.java
@@ -4,6 +4,40 @@ public class PalindromePartitioning {
 	public List<List<String>> partition(String s){
 		List<List<String>> result = new ArrayList<>();
 		if(s == null || s.length() == 0) return result;
-		return null;
+		if(s.length() == 1) {
+			List<String> stringList = new ArrayList<>();
+			stringList.add(s);
+			result.add(stringList);
+			return result;
+		}
+		for(int i = s.length() - 1; i >= 0; i--) {
+			String firstPalin = s.substring(i, s.length());
+			if(isPalindrome(firstPalin)) {
+				List<List<String>> remainLists = partition(s.substring(0, i));
+				if(remainLists.size() == 0) {
+					List<String> stringList = new ArrayList<>();
+					stringList.add(firstPalin);
+					result.add(stringList);
+					continue;
+				}
+				for(int j = 0; j < remainLists.size(); j++) {
+//					remainLists.get(j).add(0, firstPalin);
+					remainLists.get(j).add(firstPalin);
+					result.add(remainLists.get(j));
+				}
+
+			}
+		}
+		return result;
+		
+	}
+	
+	private boolean isPalindrome(String s) {
+		int head = 0;
+		int tail = s.length() - 1;
+		while(head <= tail) {
+			if(s.charAt(head++) != s.charAt(tail--)) return false;
+		}
+		return true;
 	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-//		PalindromePartitioning test = new PalindromePartitioning();
-//		List<List<String>> result = test.partition("aab");
-//		for(int i = 0; i < result.size(); i++) {
-//			System.out.println(result.get(i));
-//		}
-		AddBinary test = new AddBinary();
-		System.out.println(test.addBinary("0", "0"));
+		PalindromePartitioning test = new PalindromePartitioning();
+		List<List<String>> result = test.partition("aab");
+		for(int i = 0; i < result.size(); i++) {
+			System.out.println(result.get(i));
+		}
+
 	}
 }
