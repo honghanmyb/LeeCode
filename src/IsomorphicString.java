9fac763bc59980568b32201ea1d610cd66b15e7a
honghanmyb <han.hong@mail.utoronto.ca>
2019-02-03 23:03:38 -0500

    IsomorphicString
    Using array as hashmap
    There are 256 char in total

--- src/IsomorphicString.java
+++ src/IsomorphicString.java
@@ -2,19 +2,20 @@ import java.util.*;
 public class IsomorphicString {
 	public boolean isIsomorphic(String s, String t) {
 		if(s.length() == 0) return true;
-		HashMap<Character, Character> map1 = new HashMap<>();
-		HashMap<Character, Character> map2 = new HashMap<>();
-		
-		for(int i = 0; i < s.length(); i++) {
-			if(!map1.containsKey(s.charAt(i)) && !map2.containsKey(t.charAt(i))) {
-				map1.put(s.charAt(i), t.charAt(i));
-				map2.put(t.charAt(i), s.charAt(i));
+		char[] map1 = new char[256];
+		char[] map2 = new char[256];
+		for(int i = 0; i < map1.length; i++) {
+			map1[i] = 0;
+			map2[i] = 0;
+		}
+
+		for(int i = 0; i < t.length(); i++) {
+			if(map1[s.charAt(i)] == t.charAt(i) && map2[t.charAt(i)] == s.charAt(i)) continue;
+			if(map1[s.charAt(i) ] == 0 && map2[t.charAt(i) ] == 0) {
+				map1[s.charAt(i) ] = t.charAt(i);
+				map2[t.charAt(i) ] = s.charAt(i);
 				continue;
 			}
-			if((map1.containsKey(s.charAt(i)) && map2.containsKey(t.charAt(i))) == false) return false;
-			if(s.charAt(i) == map2.get(t.charAt(i)) &&
-					t.charAt(i) == map1.get(s.charAt(i))) continue;
-			
 			return false;
 		}
 		return true;
