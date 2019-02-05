909c39d771a3360ab1aaf43fdde97a5536c8e81c
honghanmyb <han.hong@mail.utoronto.ca>
2019-02-05 12:50:06 -0500

    Implement Trie

---
+++ src/TrieNode.java
@@ -0,0 +1,18 @@
+
+public class TrieNode {
+	private int numChar = 26;
+	
+	public char ch;
+	
+	public TrieNode[] chList;
+	
+	public TrieNode() {
+		chList = new TrieNode[numChar];
+		
+	}
+	
+	public TrieNode(char ch) {
+		this.ch = ch;
+		chList = new TrieNode[numChar];
+	}
+}
