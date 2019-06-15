f39ec38b9b80bb702c9f4cbda009d56f06730baa
honghanmyb <han.hong@mail.utoronto.ca>
2019-06-15 00:28:09 -0400

    Leecode 236

--- src/LCA.java
+++ src/LCA.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class LCA {
-
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		List<TreeNode> pRoute = new ArrayList<>();
+		List<TreeNode> qRoute = new ArrayList<>();
+		findNodeRoute(p, root, pRoute);
+		findNodeRoute(q, root, qRoute);
+		for(int i = 0; i < Math.min(pRoute.size(), qRoute.size()) - 1; i++) {
+			if(pRoute.get(i + 1) != qRoute.get(i + 1)) {
+				return pRoute.get(i);
+			}
+		}
+		return pRoute.get(Math.min(pRoute.size(), qRoute.size()) - 1);
+	}
+	
+	private boolean findNodeRoute(TreeNode child, TreeNode ancester, List<TreeNode> route) {
+		route.add(ancester);
+		if(ancester == child) return true;
+		if(ancester == null) return false;
+		if(findNodeRoute(child, ancester.left, route)) {
+			return true;
+		}
+		if(findNodeRoute(child, ancester.right, route)) {
+			return true;
+		}
+		route.remove(ancester);
+		return false;
+	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,12 +1,18 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		Trie test = new Trie();
-		test.insert("apple");
-		System.out.println(test.search("apple"));
-		System.out.println(test.search("appl"));
-		System.out.println(test.startsWith("app"));
-		test.insert("app");
-		System.out.println(test.search("app"));
+		LCA test = new LCA();
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(5);
+		root.left.left = new TreeNode(6);
+		root.left.right = new TreeNode(2);
+		root.left.right.left = new TreeNode(7);
+		root.left.right.right = new TreeNode(4);
+		root.right = new TreeNode(1);
+		root.right.left = new TreeNode(0);
+		root.right.right = new TreeNode(8);
+		TreeNode p = root.left;
+		TreeNode q = root.left.right.right;
+		System.out.println(test.lowestCommonAncestor(root, p, q).val);
 	}
 }
