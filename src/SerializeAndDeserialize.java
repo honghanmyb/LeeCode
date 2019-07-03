d0f884b9048af62a3109f2634fda5a33d913652c
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-03 00:10:37 -0400

    Leecode 297 serialize and deserialize tree

--- src/SerializeAndDeserialize.java
+++ src/SerializeAndDeserialize.java
@@ -1,57 +1,39 @@
 import java.util.*;
 public class SerializeAndDeserialize {
+	private int currentIndex = 0;
 	public String serialize(TreeNode root) {
-		Queue<TreeNode> levelOrderQueue = new LinkedList<>();
 		List<Integer> serializeData = new ArrayList<>();
-		levelOrderQueue.add(root);
-		while(serializeData.size() < Math.pow(2, getTreeLevel(root, 0)) - 1) {
-			TreeNode currentHead = levelOrderQueue.poll();
-			if(currentHead != null) {
-				levelOrderQueue.add(currentHead.left);
-				levelOrderQueue.add(currentHead.right);
-				serializeData.add(currentHead.val);
-			}else {
-				levelOrderQueue.add(null);
-				levelOrderQueue.add(null);
-				serializeData.add(null);
-			}
-		}
+		preorderTraversal(root, serializeData);
 		return serializeData.toString();
 	}
 	
 	public TreeNode deserialize(String data) {
 		if(data.equals("[]")) return null;
 		String[] treeNodeVals = data.replace("[", "").replace("]", "").replaceAll(" ", "").split(",");
-		
-		if(treeNodeVals.length == 1) return new TreeNode(Integer.parseInt(treeNodeVals[0]));
-		
-		TreeNode[] treeNodes = new TreeNode[treeNodeVals.length];
-		for(int i = 0; i < treeNodeVals.length / 2; i++) {
-			if(!treeNodeVals[i].equals("null")) {
-				treeNodes[i] = new TreeNode(Integer.parseInt(treeNodeVals[i]));
-				treeNodes[i * 2 + 1] = treeNodeVals[i * 2 +1].equals("null")? null:
-					new TreeNode(Integer.parseInt(treeNodeVals[i * 2 + 1]));
-				treeNodes[i * 2 + 2] = treeNodeVals[i * 2 + 2].equals("null")? null:
-					new TreeNode(Integer.parseInt(treeNodeVals[i * 2 + 2]));
-				treeNodes[i].left = treeNodes[i * 2 + 1];
-				treeNodes[i].right = treeNodes[i * 2 + 2];
-			}
-		}
-		
-//		for(int i = 0; i < treeNodes.length / 2; i++) {
-//			if(treeNodes[i] != null) {
-//				treeNodes[i].left = treeNodes[i * 2 + 1];
-//				treeNodes[i].right = treeNodes[i * 2 + 2];
-//			}
-//		}
-		return treeNodes[0];
+
+		TreeNode root = reconstructTree(treeNodeVals);
+		return root;
 	}
 	
-	private int getTreeLevel(TreeNode currentNode, int currentLevel) {
-		if(currentNode != null) {
-			return Math.max(getTreeLevel(currentNode.left, currentLevel + 1),
-					getTreeLevel(currentNode.right, currentLevel + 1));
+	private void preorderTraversal(TreeNode currentNode, List<Integer> serializeData) {
+		if(currentNode == null) {
+			serializeData.add(null);
+			return;
 		}
-		return currentLevel;
+		serializeData.add(currentNode.val);
+		preorderTraversal(currentNode.left, serializeData);
+		preorderTraversal(currentNode.right, serializeData);
+	}
+	
+	private TreeNode reconstructTree(String[] treeNodeVals) {
+		if(currentIndex >= treeNodeVals.length) return null;
+		if(treeNodeVals[currentIndex].equals("null")) return null;
+		TreeNode currentNode = new TreeNode(Integer.parseInt(treeNodeVals[currentIndex]));
+		currentIndex += 1;
+		currentNode.left = reconstructTree(treeNodeVals);
+		currentIndex += 1;
+		currentNode.right = reconstructTree(treeNodeVals);
+		
+		return currentNode;
 	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,22 +1,17 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-//		TreeNode root = new TreeNode(1);
-//		root.left = new TreeNode(2);
-//		root.right = new TreeNode(3);
-//		root.right.right = new TreeNode(5);
-//		root.right.left = new TreeNode(4);
-//		root.right.left.right = new TreeNode(6);
-//		
-//		SerializeAndDeserialize test = new SerializeAndDeserialize();
-//		String serialize = test.serialize(root);
-//		System.out.println(serialize);
-////		test.deserialize(serialize);
-//		System.out.println(test.serialize(test.deserialize(serialize)));
-		UglyNumber test = new UglyNumber();
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.right.right = new TreeNode(5);
+		root.right.left = new TreeNode(4);
+		root.right.left.right = new TreeNode(6);
 		
-		System.out.println(test.isUgly(6));
-		System.out.println(test.isUgly(8));
-		System.out.println(test.isUgly(14));
+		SerializeAndDeserialize test = new SerializeAndDeserialize();
+		String serialize = test.serialize(root);
+		System.out.println(serialize);
+//		test.deserialize(serialize);
+		System.out.println(test.serialize(test.deserialize(serialize)));
 	}
 }
