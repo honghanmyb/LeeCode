63222a1ba6250cd4ec0b735687c6b1c3dcb9d66c
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-06 00:49:41 -0400

    Leecode 141 Linked list cycle, use two runner thought

--- src/LinkedListCycle.java
+++ src/LinkedListCycle.java
@@ -2,15 +2,34 @@ import java.util.HashSet;
 
 public class LinkedListCycle {
 	public boolean hasCycle(ListNode head) {
-		HashSet<ListNode> set = new HashSet<>();
-		ListNode temp = head;
-		while(temp != null) {
-			if(set.contains(temp)) {
-				return true;
+//		HashSet<ListNode> set = new HashSet<>();
+//		ListNode temp = head;
+//		while(temp != null) {
+//			if(set.contains(temp)) {
+//				return true;
+//			}
+//			set.add(temp);
+//			temp = temp.next;
+//		}
+//		return false;
+		if(head == null) return false;
+		if(head.next == null) return false;
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while(slow != fast) {
+			if(fast.next != null) {
+				fast = fast.next.next;
+			} else {
+				return false;
 			}
-			set.add(temp);
-			temp = temp.next;
+			if(slow != null) {
+				slow = slow.next;
+			}
+			if(fast == null) {
+				return false;
+			}
 		}
-		return false;
+		
+		return true;
 	}
 }
