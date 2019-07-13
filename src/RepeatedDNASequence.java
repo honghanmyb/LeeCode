4a68c0900c0ab4364e4d2482bd88cfa24dbacc74
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-13 10:50:09 -0400

    Leecode 187 repeated DNA sequence, using two HashSet

--- src/RepeatedDNASequence.java
+++ src/RepeatedDNASequence.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
 
 public class RepeatedDNASequence {
+	public List<String> findRepeatedDnaSequence(String s){
+		HashSet<String> repeatedTable = new HashSet<>();
+		HashSet<String> resultTable = new HashSet<>();
+		int beginIndex = 0;
+		int endIndex = 10;
+		while(endIndex < s.length()) {
+			String key = s.substring(beginIndex++, endIndex++);
+			if(repeatedTable.contains(key)) {
+				repeatedTable.remove(key);
+				resultTable.add(key);
+				continue;
+			}
+			
+			repeatedTable.add(key);
+		}
+		List<String> repeatedDnaSequence = new ArrayList<>();
+		resultTable.forEach(key -> repeatedDnaSequence.add(key));
 
+		return repeatedDnaSequence;
+	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,11 +1,9 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		int[] linkedListArray = {-129, -129, -128};
-		ListNode head = TestUtils.constructLinkedListFromArray(linkedListArray);
-		PalindromeLinkedList test = new PalindromeLinkedList();
+		RepeatedDNASequence test = new RepeatedDNASequence();
+		String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
 		
-		System.out.println(test.isPalindrome(head));
-//		System.out.println(-129 != -129);
+		System.out.println(test.findRepeatedDnaSequence(s));
 	}
 }
