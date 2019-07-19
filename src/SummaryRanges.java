0eab858d23b9bb0fafcdaaeb5596186a0b317b8d
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-18 21:57:22 -0400

    Leecode 228 summary ranges

---
+++ src/SummaryRanges.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+	public List<String> summaryRanges(int[] nums){
+		List<String> ranges = new ArrayList<>();
+		if(nums.length == 0) {
+			return ranges;
+		}
+		//use a for loop
+		//three variables: start num, current num, longestRangeNum
+		int rangeStartNum = nums[0], rangeCurrentNum = nums[0];
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] - rangeCurrentNum == 1) {
+				rangeCurrentNum = nums[i];
+				continue;
+			}
+			
+			addNewRange(ranges, rangeStartNum, rangeCurrentNum);
+			rangeCurrentNum = rangeStartNum = nums[i];
+		}
+		
+		addNewRange(ranges, rangeStartNum ,rangeCurrentNum);
+		return ranges;
+	}
+	
+	private void addNewRange(List<String> ranges, int rangeStartNum, int rangeCurrentNum) {
+		if(rangeCurrentNum == rangeStartNum) {
+			ranges.add(Integer.toString(rangeStartNum));
+			return;
+		}
+		
+		ranges.add(String.format("%s->%s", Integer.toString(rangeStartNum),
+					Integer.toString(rangeCurrentNum)));
+	}
+}
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,9 +1,8 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		RepeatedDNASequence test = new RepeatedDNASequence();
-		String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
-		
-		System.out.println(test.findRepeatedDnaSequence(s));
+		SummaryRanges test = new SummaryRanges();
+		int[] nums = {0,1,2,4,5,7};
+		System.out.println(test.summaryRanges(nums));
 	}
 }
