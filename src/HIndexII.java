e5aee83e8a222ecf0dd9539e9316853993c832ba
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-24 00:17:51 -0400

    change format

--- src/HIndexII.java
+++ src/HIndexII.java
@@ -3,18 +3,20 @@ import java.util.List;
 
 public class HIndexII {
 	public int hIndex(int[] citations) {
-        if(citations.length == 0) {
-            return 0;
-        }
         int possibleHIndex = 0;
 		int N = citations.length;
-		for(int i = 1; i <= N; i++) {
-			if(citations[N - i] >= i) {
-				if(N == i || citations[N - i - 1] <= i) {
-					possibleHIndex = i;
-				}
+        if(N == 0) {
+            return 0;
+        }
+        if(citations[0] >= N) {
+        	return N;
+        }
+		for(int i = 1; i < N; i++) {
+			if(citations[N - i] >= i && citations[N - i - 1] <= i) {
+				possibleHIndex = i;
 			}
 		}
+		
 		return possibleHIndex;
 	}
 }
