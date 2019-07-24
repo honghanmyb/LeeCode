2af84a7604917a935837cb93d2ff40b24f26de1c
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-24 00:47:07 -0400

    Leecode 111 use minDepth to do recursion

--- src/BSTMinDepth.java
+++ src/BSTMinDepth.java
@@ -1,30 +1,11 @@
 
 public class BSTMinDepth {
-	private int minDepth = Integer.MAX_VALUE;
 	public int minDepth(TreeNode root) {
-		int currentLevel = 1;
 		if(root == null) {
 			return 0;
 		}
-		traversalTree(root, currentLevel);
-		return minDepth;
-	}
-	
-	private void traversalTree(TreeNode treeNode, int currentLevel) {
-		if(currentLevel >= minDepth) {
-			return;
-		}
-		TreeNode leftNode = treeNode.left;
-		TreeNode rightNode = treeNode.right;
-		if(leftNode == null && rightNode == null) {
-			minDepth = Math.min(minDepth, currentLevel);
-			return;
-		}
-		if(leftNode != null) {
-			traversalTree(leftNode, currentLevel + 1);
-		}
-		if(rightNode != null) {
-			traversalTree(rightNode, currentLevel + 1);
-		}
+		int leftMin = minDepth(root.left);
+		int rightMin = minDepth(root.right);
+		return (leftMin * rightMin == 0 ? Math.max(leftMin, rightMin) : Math.min(leftMin, rightMin)) + 1;
 	}
 }
