80cb2f3316980d02a2c28058c23c14f628b9fd19
honghanmyb <han.hong@mail.utoronto.ca>
2019-07-29 20:00:55 -0400

    remove print debug line

--- src/CountCompleteTreeNodes.java
+++ src/CountCompleteTreeNodes.java
@@ -5,7 +5,6 @@ public class CountCompleteTreeNodes {
 			return 0;
 		}
 		if(root.left == null && root.right == null) {
-			System.out.println(root.val);
 			return 1;
 		}
 		return countNodes(root.left) + countNodes(root.right) + 1;
