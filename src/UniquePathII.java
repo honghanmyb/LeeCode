897ccd5737a00d76fd3c955dfb9eec44511453b2
honghanmyb <han.hong@mail.utoronto.ca>
2019-08-27 00:57:01 -0400

    Leecode 63 unique paths II, using DP

--- src/UniquePathII.java
+++ src/UniquePathII.java
@@ -1,24 +1,38 @@
 
 public class UniquePathII {
-	private int totalUniquePaths = 0;
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		findUniquePaths(obstacleGrid, 0, 0);
-		return totalUniquePaths;
-	}
-	
-	private void findUniquePaths(int[][] obstacleGrid, int row, int column) {
-		if(obstacleGrid[row][column] == 1) {
-			return;
+		int[][] paths = new int[obstacleGrid.length][obstacleGrid[0].length];
+		boolean canReach = true;
+		for(int i = 0; i < paths.length; i++) {
+			if(canReach) {
+				if(obstacleGrid[i][0] == 0) {
+					paths[i][0] = 1;
+				}else {
+					paths[i][0] = 0;
+					canReach = false;
+				}
+			}
 		}
-		if(row == obstacleGrid.length - 1 && column == obstacleGrid[0].length - 1) {
-			totalUniquePaths += 1;
-			return;
+		canReach = true;
+		for(int j = 0; j < paths[0].length; j++) {
+			if(canReach) {
+				if(obstacleGrid[0][j] == 0) {
+					paths[0][j] = 1;
+				}else {
+					paths[0][j] = 0;
+					canReach = false;
+				}
+			}
 		}
-		if(row < obstacleGrid.length - 1) {
-			findUniquePaths(obstacleGrid, row + 1, column);
+		for(int i = 1; i < paths.length; i++) {
+			for(int j = 1; j < paths[0].length; j++) {
+				if(obstacleGrid[i][j] == 1) {
+					paths[i][j] = 0;
+				}else {
+					paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
+				}
+			}
 		}
-		if(column < obstacleGrid[0].length - 1) {
-			findUniquePaths(obstacleGrid, row, column + 1);
-		}
+		return paths[paths.length - 1][paths[0].length - 1];
 	}
 }
