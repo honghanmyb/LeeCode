78383db743fb160a1c764d48d8a1a12948b62f78
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-07 19:12:26 -0400

    Leecode 142 use two pointers, and a little math

--- src/LinkedListCycleII.java
+++ src/LinkedListCycleII.java
@@ -3,15 +3,41 @@ import java.util.Set;
 
 public class LinkedListCycleII {
 	public ListNode detectCycle(ListNode head) {
-		Set<ListNode> set = new HashSet<>();
-		ListNode startNode = null;
-		while(head != null) {
-			if(!set.add(head)) {
-				startNode = head;
-				break;
-			}
-			head = head.next;
+		ListNode slowRunner, fastRunner;
+		fastRunner = hasCycle(head);
+		if(fastRunner == null) {
+			return null;
 		}
-		return startNode;
+		slowRunner = head;
+		while(fastRunner != slowRunner) {
+			fastRunner = fastRunner.next;
+			slowRunner = slowRunner.next;
+		}
+		return fastRunner;
+	}
+	
+	//assumes a linked list has cycle, distance from head to startNode = x, distance from startNode to 
+	//slow and fast runner meets = y, from meet point to end of list is z
+	//when slow and fast runner meets, if slow travels t, then fast travels 2t(because slow speed is 1, fast is 2)
+	//so t = x + y, 2t = x + y + z + y
+	//so x = z, which means from head to startNode equals from meet point to end of list
+	private ListNode hasCycle(ListNode head) {
+		ListNode slowRunner, fastRunner;
+		slowRunner = head.next;
+		if(slowRunner == null) {
+			return null;
+		}
+		fastRunner = slowRunner.next;
+		if(fastRunner == null) {
+			return null;
+		}
+		while(fastRunner != slowRunner) {
+			if(fastRunner == null || slowRunner == null ||fastRunner.next == null) {
+				return null;
+			}
+			fastRunner = fastRunner.next.next;
+			slowRunner = slowRunner.next;
+		}
+		return fastRunner;
 	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
 		LinkedListCycleII test = new LinkedListCycleII();
-		ListNode head = TestUtils.constructLinkedListFromArray(new int[] {3, 2, 0, 4});
-		head.next.next.next.next = head.next;
+		ListNode head = TestUtils.constructLinkedListFromArray(new int[] {-1,-7,7,-4,19,6,-9,-5,-2,-5});
+		head.next.next.next.next.next.next.next.next.next = head.next;
 		System.out.println(test.detectCycle(head).val);
 	}
 }
