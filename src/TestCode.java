749e018351d44f7acd015a2cc3a10d100740e162
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-08 13:37:03 -0400

    Leecode 164 use bucket sort, MLE -- memory limit exceed

--- src/TestCode.java
+++ src/TestCode.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		EvaluateRPN test = new EvaluateRPN();
-		String[] tokens = new String[] {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
-		System.out.println(test.evalRPN(tokens));
+		MaximumGap test = new MaximumGap();
+		int[] nums = new int[] {2,99999999};
+		System.out.println(test.maximumGap(nums));
 	}
 }
