5f8cd91d838bc2eb10b7b55d0df7190d21ab123d
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-08 11:57:27 -0400

    LeeCode 150 evaluate reverse polish notation, using stack

---
+++ src/EvaluateRPN.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class EvaluateRPN {
+	public int evalRPN(String[] tokens) {
+		Stack<Integer> stack = new Stack<>();
+		int firstInt, secondInt;
+		for(String token: tokens) {
+			if(token.equals("+")) {
+				secondInt = stack.pop();
+				firstInt = stack.pop();
+				stack.add(firstInt + secondInt);
+			}else if(token.equals("-")) {
+				secondInt = stack.pop();
+				firstInt = stack.pop();
+				stack.add(firstInt - secondInt);
+			}else if(token.equals("*")) {
+				secondInt = stack.pop();
+				firstInt = stack.pop();
+				stack.add(firstInt * secondInt);
+			}else if(token.equals("/")) {
+				secondInt = stack.pop();
+				firstInt = stack.pop();
+				stack.add(firstInt / secondInt);
+			}else {
+				stack.add(Integer.parseInt(token));
+			}
+		}
+		
+		return stack.peek();
+	}
+}
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,9 +1,8 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		LinkedListCycleII test = new LinkedListCycleII();
-		ListNode head = TestUtils.constructLinkedListFromArray(new int[] {-1,-7,7,-4,19,6,-9,-5,-2,-5});
-		head.next.next.next.next.next.next.next.next.next = head.next;
-		System.out.println(test.detectCycle(head).val);
+		EvaluateRPN test = new EvaluateRPN();
+		String[] tokens = new String[] {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+		System.out.println(test.evalRPN(tokens));
 	}
 }
