3b3af7e4430dda1e2a9991adac87e74559b41b3e
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-18 14:38:11 -0400

    Leecode 76 minimum window sbustring, using arrays to record window coordinate, use set to check if a char is in the string

--- src/MinimumWindowSubstring.java
+++ src/MinimumWindowSubstring.java
@@ -1,50 +1,61 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MinimumWindowSubstring {
 	public String minWindow(String s, String t) {
 		if(t.isEmpty() || s.isEmpty()) {
 			return "";
 		}
+		Set<Character> tSet = new HashSet<>();
 		int[] chars = new int[256];
 		for(int i = 0; i < t.length(); i++) {
 			chars[t.charAt(i)]++;
+			tSet.add(t.charAt(i));
 		}
 		int head = 0, tail = 0;
-		String minString = s;
-		String currentString = s.substring(head, tail);
+		int[] minStringRecord = new int[] {0, s.length() - 1};
+		int[] currentStringRecord = new int[] {head, tail};
+		
 		boolean findWindow = false;
 		while(true) {
 			if(!findWindow) {
 				if(tail >= s.length()) {
 					break;
 				}
+				currentStringRecord[1] = tail;
 				char currentCh = s.charAt(tail++);
-				currentString += currentCh;
-				if(t.indexOf(currentCh) >= 0) {
+				if(tSet.contains(currentCh)) {
 					chars[currentCh]--;
 					if(findWindow(chars, t)) {
 						findWindow = true;
-						minString = minString.length() > currentString.length()? currentString: minString;
+						if(minStringRecord[1] - minStringRecord[0] > currentStringRecord[1] - currentStringRecord[0]) {
+							minStringRecord[0] = currentStringRecord[0];
+							minStringRecord[1] = currentStringRecord[1];
+						}
 						continue;
 					}
 				}
 			}else {
 				char currentCh = s.charAt(head++);
-				currentString = currentString.substring(1);
-				if(t.indexOf(currentCh) >= 0) {
+				currentStringRecord[0] = head;
+				if(tSet.contains(currentCh)) {
 					chars[currentCh]++;
 					if(chars[currentCh] > 0) {
 						findWindow = false;
 						continue;
 					}
 				}
-				minString = minString.length() > currentString.length()? currentString: minString;
+				if(minStringRecord[1] - minStringRecord[0] > currentStringRecord[1] - currentStringRecord[0]) {
+					minStringRecord[0] = currentStringRecord[0];
+					minStringRecord[1] = currentStringRecord[1];
+				}
 			}
 		}
 		if(head == 0) {
 			return "";
 		}
-		return minString;
+		return s.substring(minStringRecord[0], minStringRecord[1] + 1);
 	}
 	
 	private boolean findWindow(int[] chars, String t) {
