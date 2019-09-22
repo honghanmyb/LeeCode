b266fd456b5bc866af9e34f9aff5bc995097bb00
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-21 23:49:03 -0400

    Leecode 124 use instance variable instead of HashMap to record max path sum

--- src/BSTMaximumPathSum.java
+++ src/BSTMaximumPathSum.java
@@ -3,39 +3,29 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class BSTMaximumPathSum {
-	public int maxPathSum(TreeNode root) {
-        Map<TreeNode, Integer> treeNodeToPathSum = new HashMap<>();
-        int maxPathSum = root.val;
-        addAllPathSum(root, treeNodeToPathSum);
-        int left = root.left == null? 0: treeNodeToPathSum.get(root.left);
-        int right = root.right == null? 0: treeNodeToPathSum.get(root.right);
-        maxPathSum += left > 0? left: 0;
-        maxPathSum += right > 0? right: 0;
-        for(Entry<TreeNode, Integer> entry: treeNodeToPathSum.entrySet()){
-            TreeNode currentNode = entry.getKey();
-            int currentPathSum = currentNode.val;
-            left = currentNode.left == null? 0: treeNodeToPathSum.get(currentNode.left);
-            right = currentNode.right == null? 0: treeNodeToPathSum.get(currentNode.right);
-            currentPathSum += left > 0? left: 0;
-            currentPathSum += right > 0? right: 0;
-            maxPathSum = Integer.max(maxPathSum, currentPathSum);
-        }
+    private int maxPathSum = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        addAllPathSum(root);
         return maxPathSum;
-	}
-	
-    private int addAllPathSum(TreeNode treeNode, Map<TreeNode, Integer> treeNodeToPathSum){
+    }
+    
+    private int addAllPathSum(TreeNode treeNode){
         if(treeNode == null){
             return 0;
         }
-        int left = addAllPathSum(treeNode.left, treeNodeToPathSum);
-        int right = addAllPathSum(treeNode.right, treeNodeToPathSum);
+        int currentPathSum = treeNode.val;
+        int left = addAllPathSum(treeNode.left);
+        int right = addAllPathSum(treeNode.right);
+        currentPathSum += left > 0? left: 0;
+        currentPathSum += right > 0? right: 0;
+        maxPathSum = Integer.max(maxPathSum, currentPathSum);
         int currentSum = Integer.max(left, right);
         if(currentSum <= 0){
             currentSum = treeNode.val;
         }else{
             currentSum += treeNode.val;
         }
-        treeNodeToPathSum.put(treeNode, currentSum);
         return currentSum;
     }
 }
