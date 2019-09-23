6ef603045f9f6de69fad2081fa2f96ce006309cc
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-23 01:07:42 -0400

    Leecode 45 add a new boolean array to indicate if a pos can be reach before, greatly improve time

--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -6,13 +6,19 @@ public class JumpGameII {
             return 0;
         }
         int[] jumpTimes = new int[nums.length];
-        Arrays.fill(jumpTimes, Integer.MAX_VALUE);
+        boolean[] canJump = new boolean[nums.length];
         jumpTimes[0] = 0;
+        canJump[0] = true;
         int currentFarest = 0;
+        int lastFarest = 0;
         for(int i = 0; i < nums.length; i++){
             currentFarest = i + nums[i];
-            for(int j = i; j <= Integer.min(currentFarest, nums.length - 1); j++){
-                jumpTimes[j] = Integer.min(jumpTimes[j], jumpTimes[i] + 1);
+            for(int j = lastFarest + 1; j <= Integer.min(currentFarest, nums.length - 1); j++){
+                if(canJump[j] == false){
+                    jumpTimes[j] = jumpTimes[i] + 1;
+                    canJump[j] = true;
+                }
+                lastFarest = currentFarest;
             }
         }
         return jumpTimes[nums.length - 1];
