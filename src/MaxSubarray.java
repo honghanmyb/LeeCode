0c63b27f3292cf27b7421a601ffebc3cb0b5277e
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-23 00:13:58 -0400

    leecode 53 rewrite  maximum subarray using DP

--- src/MaxSubarray.java
+++ src/MaxSubarray.java
@@ -1,24 +1,15 @@
 
 public class MaxSubarray {
 	public int maxSubArray(int[] nums) {
-		int result = nums[0] ;int max = nums[0] ;
-		for(int i = 0; i < nums.length; i++) {
-			if(nums[i] > 0) {
-				result = max= 0;
-				for(int j = 0; j < nums.length; j++) {
-					if(result + nums[j] > 0) result += nums[j];
-					else result = 0;
-					if(result > max) max = result;
-				}
-				return max;
-			}
-		}
-		
-		for(int i = 1; i < nums.length; i++) {
-			if(result + nums[i] > 0) result += nums[i];
-			else result = nums[i];
-			if(result > max) max = result;
-		}
-		return max;
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            currentSum += nums[i];
+            maxSum = Integer.max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
 	}
 }
