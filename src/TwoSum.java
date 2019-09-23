f27b196d955504185215adc2077f0b57fceb423a
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-23 15:06:22 -0400

    Leecode 1 TwoSum update, using a HashMap

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,21 +1,13 @@
 import java.util.*;
 public class TwoSum {
 	public int[] twoSum(int[] nums, int target) {
-		int[] result = new int[2];
-		HashMap<Integer, Integer> record = new HashMap<Integer, Integer>();
-		for(int i = 0; i < nums.length; i++) {
-			if(record.containsKey(nums[i])) {
-				if(nums[record.get(nums[i])] + nums[i] == target) {
-					result[0] = record.get(nums[i]);
-					result[1] = i;
-					return result;
-				}
-
-			}
-			record.put(nums[i], i);
-			record.put(target - nums[i], i);
-		}
-		
-		return result;
+        Map<Integer, Integer> valueToIndices  = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(valueToIndices.containsKey(target - nums[i])){
+                return new int[]{i, valueToIndices.get(target - nums[i])};
+            }
+            valueToIndices.put(nums[i], i);
+        }
+        return null;
 	}
 }
