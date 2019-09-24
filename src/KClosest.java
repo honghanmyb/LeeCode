56dc807631673b8dad661646d62e5d61d84d75ae
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-24 13:02:11 -0400

    Leecode 973 K Closest Points to Origin, using quick select

--- src/KClosest.java
+++ src/KClosest.java
@@ -1,16 +1,48 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class KClosest {
     public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, (point1, point2) -> {
-            int dis1 = point1[0] * point1[0] + point1[1] * point1[1];
-            int dis2 = point2[0] * point2[0] + point2[1] * point2[1];
-            return dis1 - dis2;
-        });
+        List<int[]> dists = new ArrayList<>(points.length);
+        for(int i = 0; i < points.length; i++){
+            int[] dist = new int[2];
+            dist[0] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            dist[1] = i;
+            dists.add(dist);
+        }
+        int kDist = quickSelect(dists, K);
         int[][] kClosest = new int[K][2];
-        for(int i = 0; i < K; i++){
-            kClosest[i] = points[i];
+        int count = 0;
+        for(int[] dist: dists){
+            if(dist[0] <= kDist){
+                kClosest[count++] = points[dist[1]];
+            }
         }
         return kClosest;
+    }
+    
+    private int quickSelect(List<int[]> dists, int K){
+        List<int[]> small = new ArrayList<>();
+        List<int[]> large = new ArrayList<>();
+        int[] flagElement = dists.get(dists.size() - 1);
+        for(int[] dist: dists){
+            if(dist == flagElement){
+                continue;
+            }
+            if(dist[0] <= flagElement[0]){
+                small.add(dist);
+            }else{
+                large.add(dist);
+            }
+        }
+        if(small.size() > K - 1){
+            return quickSelect(small, K);
+        }
+        if(small.size() < K - 1){
+            return quickSelect(large, K - small.size() - 1);
+        }else{
+            return flagElement[0];
+        }
     }
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,18 +1,10 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		SpiralMatrix test = new SpiralMatrix();
-//		int[][] matrix = new int[][] {
-//			{1, 2, 3, 4, 5},
-//			{6, 7, 8, 9, 10},
-//			{11, 12, 13, 14, 15},
-//			{16, 17, 18, 19, 20}
-//		};
-		int[][] matrix = new int[][] {
-			{7},
-			{9},
-			{6}
+		KClosest test = new KClosest();
+		int[][] points = new int[][] {
+			{1, 3}, {2, 2}
 		};
-		System.out.println(test.spiralOrder(matrix));
+		System.out.println(test.kClosest(points, 1));
 	}
 }
