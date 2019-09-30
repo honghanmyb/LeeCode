e2b455939a6ed3f08b220c41f22735b8c4ea72f7
honghanmyb <han.hong@mail.utoronto.ca>
2019-09-30 12:41:40 -0400

    Leecode 169 Majority Element, using BM Voting

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 public class MajorityElement {
 	public int majorityElement(int[] nums) {
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for(int i = 0; i < nums.length ; i++) {
-			if(map.containsKey(nums[i]) == false) {
-				map.put(nums[i], 1);
-			}else {
-				map.put(nums[i], map.get(nums[i]) +1);
-			}
-			
-		}
-		int key = nums[0], maxValue = map.get(nums[0]);
-		Iterator<Integer> keys = map.keySet().iterator();
-		while(keys.hasNext()) {
-			int temp = (int)keys.next();
-			if(map.get(temp) > maxValue) {
-				key = temp;
-				maxValue = map.get(temp);
-			}
-		}
-
-		return key;
+        if(nums.length == 0){
+            return 0;
+        }
+        int currentNum = nums[0];
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == currentNum){
+                count++;
+            }else{
+                count--;
+            }
+            if(count < 0){
+                currentNum = nums[i];
+                count = 0;
+            }
+        }
+        
+        return currentNum;
 	} 
 
 }
