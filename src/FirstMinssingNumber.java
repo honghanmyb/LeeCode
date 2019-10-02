8b01811a77f000cfeacce50c81976d18df0ec150
honghanmyb <han.hong@mail.utoronto.ca>
2019-10-02 19:05:03 -0400

    Leecode 41 Fiss Missing Positive, using input nums array as hash table, nums[i] being negetive suggests i + 1 is in nums array

--- src/FirstMinssingNumber.java
+++ src/FirstMinssingNumber.java
@@ -1,39 +1,31 @@
 import java.util.*;
 public class FirstMinssingNumber {
 	public int firstMissingPositive(int[] nums) {
-		/**********HashSet solution************/
-		/*HashSet<Integer> set = new HashSet<>();
-		for(int i = 0; i < nums.length; i++) {
-			set.add(nums[i]);
-		}
-		int missingPositive = 1;
-		while(set.contains(missingPositive++)) {
-			
-		}
-		
-		return --missingPositive;*/
-		
-		/*****Building a hash set manually**********/
-		
-		
-		int[] records = new int[nums.length];
-		int missingPositive = 0;
-		for(int i = 0; i < records.length; i++) {
-			if(nums[i] > 0 && nums[i] < nums.length + 1) {
-				records[nums[i] - 1] = nums[i];
-			}
-		}
-		for(int i = 0; i < records.length; i++) {
-			if(records[i] == 0) {
-				missingPositive = i + 1;
-				break;
-			}
-		}
-		if(missingPositive == 0) {
-			missingPositive = records.length + 1;
-		}
-		
-		return missingPositive;
-		
+        boolean containsOne = false;
+        for(int num: nums){
+            if(num == 1){
+                containsOne = true;
+                break;
+            }
+        }
+        if(!containsOne){
+            return 1;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= 0 || nums[i] > nums.length){
+                nums[i] = 1;
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            int numFlag = nums[i];
+            numFlag = numFlag > 0? numFlag: -numFlag;
+            nums[numFlag - 1] = nums[numFlag - 1] > 0? -nums[numFlag - 1]: nums[numFlag - 1];
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
 	}
 }
