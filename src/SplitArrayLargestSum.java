1e10424e6965d3706cdc8fa8b45addf952a9ac83
honghanmyb <han.hong@mail.utoronto.ca>
2019-10-03 00:54:03 -0400

    Leecode 410 split arrya largest sum, add comments for using binary search

--- src/SplitArrayLargestSum.java
+++ src/SplitArrayLargestSum.java
@@ -28,4 +28,51 @@ public class SplitArrayLargestSum {
         }
         return minMaxSum[m - 1][nums.length - 1];
     }
+    
+    /**
+     *	using binary search
+     *	basic idea is to first define the max and min possible value of the sum of sub array
+     *	then find the middle possible of the sum of sub array
+     *	check if the array can be devide into m sub arrays with each sum smaller than middle
+     *	if yes, then the middle is too large, make max = middle
+     *	if no, then the middle is too small, make min = middle + 1
+     *	by doing so on and on, finally will narrow down the min and max value
+     *	then return the min value
+     *     public int splitArray(int[] nums, int m) {
+
+        int minVal = Integer.MIN_VALUE, maxVal = 0;
+        for (int num : nums) {
+            minVal = Math.max(minVal, num);
+            maxVal += num;
+        }
+
+        while (minVal < maxVal) {
+            int mid = minVal + (maxVal - minVal) / 2;
+            if (canSplit(mid, nums, m)) {
+                maxVal = mid;
+            } else {
+                minVal = mid + 1;
+            }
+        }
+
+        return minVal;
+    }
+
+    private boolean canSplit(int upperBoundSubarraySum, int[] nums, int m) {
+
+        int curSubarraySum = 0, cntSubarray = 1;
+        for (int num : nums) {
+            curSubarraySum += num;
+            if (curSubarraySum > upperBoundSubarraySum) {
+                cntSubarray++;
+                curSubarraySum = num;
+                if (cntSubarray > m) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+     */
 }
