c57b12be5102300a00fc84f28e3ad87a5fed9d72
honghanmyb <han.hong@mail.utoronto.ca>
2019-10-08 13:38:32 -0400

    Leecode 315 count of smaller number after self, using binary search tree approcach

--- src/CountOfSmallerNumbersAfterSelf.java
+++ src/CountOfSmallerNumbersAfterSelf.java
@@ -1,19 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class CountOfSmallerNumbersAfterSelf {
     public List<Integer> countSmaller(int[] nums) {
         if(nums.length == 0){
             return new ArrayList<>();
         }
-        List<Integer> numCount = new ArrayList<>(nums.length);
-        for(int i = 0; i < nums.length ; i++){
-            int count = 0;
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[j] < nums[i]){
-                    count++;
-                }
-            }
-            numCount.add(count);
+        int[] counts = new int[nums.length];
+        counts[counts.length - 1] = 0;
+        Node root = new Node(nums[nums.length - 1], 0, 0);
+        for(int i = nums.length - 2; i >= 0; i--){
+            insertNode(nums[i], root, 0, counts, i);
         }
-        return numCount;
+        List<Integer> countList = new ArrayList<>();
+        for(int count: counts){
+            countList.add(count);
+        }
+        return countList;
+    }
+    
+    private void insertNode(int val, Node node, int leftChildCount, int[] counts, int index){
+        node.previousLeftChildCount = leftChildCount;
+        if(node.val == val){
+            counts[index] = node.previousLeftChildCount + node.directLeftChildCount + node.turnRightCount;
+            node.duplicate += 1;
+            return;
+        }
+        if(node.val < val){
+            int totalLeftCount = node.previousLeftChildCount + node.directLeftChildCount + node.duplicate;
+            if(node.right == null){
+                node.right = new Node(val, totalLeftCount, node.turnRightCount + 1);
+                counts[index] = totalLeftCount + node.turnRightCount + 1;
+            }else{
+                insertNode(val, node.right, totalLeftCount, counts, index);
+            }
+            return;
+        }
+        if(node.val > val){
+            node.directLeftChildCount += 1;
+            if(node.left == null){
+                Node leftNode = new Node(val, node.previousLeftChildCount, node.turnRightCount);
+                node.left = leftNode;
+                counts[index] = leftNode.previousLeftChildCount + node.turnRightCount;
+            }else {
+                insertNode(val, node.left, node.previousLeftChildCount, counts, index);
+            }
+        }
+    }
+    
+    private class Node{
+        int val;
+        int previousLeftChildCount;
+        int directLeftChildCount;
+        int turnRightCount;
+        int duplicate;
+        Node left;
+        Node right;
+        
+        public Node(int val, int previousLeftChildCount, int turnRightCount){
+            this.val = val;
+            this.previousLeftChildCount = previousLeftChildCount;
+            this.turnRightCount = turnRightCount;
+            this.directLeftChildCount = 0;
+            this.duplicate = 0;
+        }
     }
 }
