fa7ef63f065175d68f2fb9ba4761a918bcaf084b
honghanmyb <han.hong@mail.utoronto.ca>
2019-10-09 11:14:31 -0400

    Leecode 1032 Stream of Characters, using trie, insert words into trie with a reverse order

--- src/StreamChecker.java
+++ src/StreamChecker.java
@@ -2,16 +2,35 @@
 public class StreamChecker {
     private WordTrie wordTrie;
     private String previousQueries = "";
+    private int longestWordLength = 0;
+    private String[] words;
     public StreamChecker(String[] words) {
         wordTrie = new WordTrie("#");
         wordTrie.isWord = false;
+        this.words = words;
         for(String word: words){
             wordTrie.insert(word);
+            longestWordLength = Integer.max(longestWordLength, word.length());
         }
     }
     
     public boolean query(char letter) {
-        
+        previousQueries += letter;
+        if(previousQueries.length() > longestWordLength){
+            previousQueries = previousQueries.substring(1);
+        }
+        WordTrie currentTrie = this.wordTrie;
+        for(int i = previousQueries.length() - 1; i >= 0; i--){
+            char currentCh = previousQueries.charAt(i);
+            if(currentTrie.wordList[currentCh - 'a'] != null){
+                currentTrie = currentTrie.wordList[currentCh - 'a'];
+                if(currentTrie.isWord){
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
         return false;
     }
     
@@ -21,14 +40,14 @@ public class StreamChecker {
         WordTrie[] wordList;
         
         public WordTrie(String word){
-            this.ch = word.charAt(0);
+            this.ch = word.charAt(word.length() - 1);
             this.wordList = new WordTrie[26];
             if(word.length() == 1){
                 this.isWord = true;
                 return;
             }
-            char nextCh = word.charAt(1);
-            wordList[nextCh - 'a'] = new WordTrie(word.substring(1));
+            char nextCh = word.charAt(word.length() - 2);
+            wordList[nextCh - 'a'] = new WordTrie(word.substring(0, word.length() - 1));
         }
         
         public void insert(String word){
@@ -36,23 +55,12 @@ public class StreamChecker {
                 this.isWord = true;
                 return;
             }
-            char currentCh = word.charAt(0);
+            char currentCh = word.charAt(word.length() - 1);
             if(wordList[currentCh - 'a'] != null){
-                wordList[currentCh - 'a'].insert(word.substring(1));
+                wordList[currentCh - 'a'].insert(word.substring(0, word.length() - 1));
             }else{
                 wordList[currentCh - 'a'] = new WordTrie(word);
             }
-        }
-        
-        public boolean isWord(String word){
-            if(word.isEmpty()){
-                return this.isWord;
-            }
-            char currentCh = word.charAt(0);
-            if(wordList[currentCh - 'a'] == null){
-                return false;
-            }
-            return wordList[currentCh - 'a'].isWord(word.substring(1));
         }
     }
 }
