f2c9e80da96c1f6bb22e09574100d1bf1fb3d0a7
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-11 15:40:20 -0500

    472. Concatenated Words
    using dp, finally get pass test cases, considering using trie

--- src/ConcatenatedWords.java
+++ src/ConcatenatedWords.java
@@ -1,45 +1,42 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ConcatenatedWords {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> concatenatedWords = new ArrayList<>();
-        if(words.length <= 1){
-            return concatenatedWords;
-        }
-        
+        List<String> wordList = new ArrayList<>(words.length);
         Set<String> wordSet = new HashSet<>();
         for(String word: words){
             if(word.equals("")){
                 continue;
             }
-            wordSet.add(word);
+            wordList.add(word);
         }
-        for(String word: words){
-            if(word.equals("")){
-                continue;
-            }
-            wordSet.remove(word);
-            if(isConcatenatedWord(word, wordSet)){
-                concatenatedWords.add(word);
-            }
-            wordSet.add(word);
+        Collections.sort(wordList, (word1, word2) -> word1.length() - word2.length());
+        List<String> concatenatedWordList = new ArrayList<>();
+        if(wordList.isEmpty()){
+            return concatenatedWordList;
         }
-        return concatenatedWords;
+        wordSet.add(wordList.get(0));
+        for(int i = 1; i < wordList.size(); i++){
+            if(canForm(wordList.get(i), wordSet)){
+                concatenatedWordList.add(wordList.get(i));
+            }
+            wordSet.add(wordList.get(i));
+        }
+        return concatenatedWordList;
     }
     
-    private boolean isConcatenatedWord(String word, Set<String> wordSet){
+    private boolean canForm(String word, Set<String> wordSet){
         boolean[] canForm = new boolean[word.length() + 1];
         canForm[0] = true;
         for(int i = 1; i < canForm.length; i++){
-            if(canForm[i - 1] == false){
-                continue;
-            }
-            for(int j = i; j < canForm.length; j++){
-                if(wordSet.contains(word.substring(i - 1, j))){
-                    canForm[j] = true;
+            for(int j = 0; j < i; j++){
+                if(wordSet.contains(word.substring(j, i)) && canForm[j] == true){
+                    canForm[i] = true;
+                    break;
                 }
             }
         }
