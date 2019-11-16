c48624a3bbe6984ed999587e08ab29d1a6b4e608
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-15 22:54:38 -0500

    Min domino rotate for equal row
    if we can swap the values in A, B to make the equal row, then the value in the row must be either A[0] or B[0], so we can see if A can be made equal row with value A[0] or B[0], or B can be made equal row with value A[0] or B[0], then we can return the smallest value

---
+++ src/MinDominoRotationForEqualRowOA.java
@@ -0,0 +1,28 @@
+
+public class MinDominoRotationForEqualRowOA {
+	public int minDominoRotations(int[] A, int[] B) {
+		int minRotate = Integer.MAX_VALUE;
+		minRotate = Integer.min(minRotate, rotate(A, B, A[0]));
+		minRotate = Integer.min(minRotate, rotate(A, B, B[0]));
+		minRotate = Integer.min(minRotate, rotate(B, A, A[0]));
+		minRotate = Integer.min(minRotate, rotate(B, A, B[0]));
+		
+		if(minRotate == Integer.MAX_VALUE) {
+			return -1;
+		}
+		return minRotate;
+	}
+	
+	private int rotate(int[] target, int[] support, int targetValue) {
+		int rotate = 0;
+		for(int i = 0; i < target.length; i++) {
+			if(target[i] != targetValue) {
+				if(support[i] != targetValue) {
+					return Integer.MAX_VALUE;
+				}
+				rotate++;
+			}
+		}
+		return rotate;
+	}
+}
