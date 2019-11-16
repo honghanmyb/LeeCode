bf757eb86b013608533b8ff6a7f9430efde2d104
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-15 23:35:57 -0500

    1161. Maximum Level Sum of a Binary Tree

---
+++ src/MaxLevelSum.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxLevelSum {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> currentQueue = new LinkedList<>();
+        Queue<TreeNode> nextQueue = new LinkedList<>();
+        currentQueue.add(root);
+        int maxLevelIndex = 1;
+        int maxLevelSum = root.val;
+        int currentLevel = 1;
+        while(!currentQueue.isEmpty()){
+            int currentLevelSum = 0;
+            while(!currentQueue.isEmpty()){
+                TreeNode node = currentQueue.poll();
+                if(node.left != null){
+                    nextQueue.add(node.left);
+                }
+                if(node.right != null){
+                    nextQueue.add(node.right);
+                }
+                currentLevelSum += node.val;
+            }
+            if(currentLevelSum > maxLevelSum){
+                maxLevelSum = currentLevelSum;
+                maxLevelIndex = currentLevel;
+            }
+            currentLevel++;
+            Queue<TreeNode> temp = currentQueue;
+            currentQueue = nextQueue;
+            nextQueue = temp;
+        }
+        
+        return maxLevelIndex;
+    }
+}
