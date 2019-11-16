5b869e16ffaf18caeaf60570e5799e6c6d771ab2
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-15 23:12:36 -0500

    Time to type a string
    same as leecode 1165, first use an arry of int to store every char's position, then initially put finger at 0, each time type a char, calculate the time it took, and update the pos of finger

---
+++ src/TimeToTypeAStringOA.java
@@ -0,0 +1,18 @@
+
+public class TimeToTypeAStringOA {
+    public int calculateTime(String keyboard, String word) {
+        int[] charPos = new int[26];
+        for(int i = 0; i < keyboard.length(); i++){
+            char ch = keyboard.charAt(i);
+            charPos[ch - 'a'] = i;
+        }
+        int currentPos = 0;
+        int totalTime = 0;
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            totalTime += Math.abs(charPos[ch - 'a'] - currentPos);
+            currentPos = charPos[ch - 'a'];
+        }
+        return totalTime;
+    }
+}
