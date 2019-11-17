b88e4a9c8376dd4a099e01ce65ad9da8083fa41f
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-17 00:05:48 -0500

    Leecode 975. Odd Even Jump
    use TreeMap instead of TreeSet, takes a little longer time, but the code is cleaner.
    map A[i] to i and put into map.
    good thing about map is that it updates the exsiting key, but set don't change it

--- src/OddEvenJump.java
+++ src/OddEvenJump.java
@@ -1,3 +1,4 @@
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class OddEvenJump {
@@ -6,30 +7,18 @@ public class OddEvenJump {
         boolean[] canReachEven = new boolean[A.length];
         canReachOdd[A.length - 1] = true;
         canReachEven[A.length - 1] = true;
-        TreeSet<Element> setCeiling = new TreeSet<>((e1, e2) -> {
-            return e1.value == e2.value ? e1.index - e2.index: e1.value - e2.value;
-        });
-        TreeSet<Element> setFloor = new TreeSet<>((e1, e2) -> {
-        	return e1.value == e2.value ? e2.index - e1.index: e1.value - e2.value;
-        });
-        Element firstElement = new Element(A[A.length - 1], A.length - 1);
-        setCeiling.add(firstElement);
-        setFloor.add(firstElement);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(A[A.length - 1], A.length - 1);
         for(int i = A.length - 2; i >= 0; i--){
-            Element current = new Element(A[i], i);
-            Element smallest = setCeiling.ceiling(current);
-            Element largest = setFloor.floor(current);
-            setCeiling.add(current);
-            setFloor.add(current);
-            if(smallest != null){
-                canReachOdd[i] = canReachEven[smallest.index];
-                if(smallest.value == current.value) {
-                	largest = smallest;
-                }
+            Integer small = map.ceilingKey(A[i]);
+            Integer large = map.floorKey(A[i]);
+            if(small != null){
+                canReachOdd[i] = canReachEven[map.get(small)];
             }
-            if(largest != null){
-                canReachEven[i] = canReachOdd[largest.index];
+            if(large != null){
+                canReachEven[i] = canReachOdd[map.get(large)];
             }
+            map.put(A[i], i);
         }
         int count = 0;
         for(int i = 0; i < A.length; i++){
@@ -38,15 +27,5 @@ public class OddEvenJump {
             }
         }
         return count;
-    }
-    
-    private class Element{
-        public int value;
-        public int index;
-        
-        public Element(int value, int index){
-            this.value = value;
-            this.index = index;
-        }
     }
 }
