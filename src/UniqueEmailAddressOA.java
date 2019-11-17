899655e268d36fdb3d12684e387c41d4331da527
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-16 19:58:04 -0500

    Unique email address
    first seperate local name and domain name by the appearance of "@", then convert the local name, put back the converted local name and domain name in a set, finally return the set's size

---
+++ src/UniqueEmailAddressOA.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddressOA {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmail = new HashSet<>();
+        for(String email: emails){
+            uniqueEmail.add(convertEmail(email));
+        }
+        return uniqueEmail.size();
+    }
+    
+    private String convertEmail(String email){
+        int middleIndex = email.indexOf("@");
+        String domainName = email.substring(middleIndex);
+        String localName = email.substring(0, middleIndex);
+        int plusIndex = localName.indexOf("+");
+        if(plusIndex != -1){
+            localName = localName.substring(0, plusIndex);
+        }
+        localName = localName.replaceAll("\\.", "");
+        String convertedName = localName + domainName;
+        return localName + domainName;
+    }
+}
