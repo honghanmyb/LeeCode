53b36f1ce5cd2eec3822a23fcf5b1fd4b81d4bdb
honghanmyb <han.hong@mail.utoronto.ca>
2019-11-18 00:30:19 -0500

    Min Days to Bloom
    use binary search, first check if min day can get enough bouquet, then check if max day can get enough bouquet.
    after that, check the middle, use binary search to find the smallest day, time complexity O(nlog(max - min))

--- src/MinDaysBloomOA.java
+++ src/MinDaysBloomOA.java
@@ -1,7 +1,45 @@
 
 public class MinDaysBloomOA {
 	public int minDaysBloom(int[] roses, int k, int n) {
-		
-		return 0;
+		if(roses.length < k) {
+			return -1;
+		}
+		int head = roses[0], tail = roses[0];
+		for(int rose: roses) {
+			head = Integer.min(head, rose);
+			tail = Integer.max(tail, rose);
+		}
+		if(canGetBouquet(roses, head, k, n)) {
+			return head;
+		}
+		if(!canGetBouquet(roses, tail, k, n)) {
+			return -1;
+		}
+		while(head <= tail) {
+			int mid = head + (tail - head) / 2;
+			if(canGetBouquet(roses, mid, k, n)) {
+				tail = mid - 1;
+			}else {
+				head = mid + 1;
+			}
+		}
+		return head;
+	}
+	
+	private boolean canGetBouquet(int[] roses, int currentDay, int k, int n) {
+		int bouquetCount = 0;
+		int roseCount = 0;
+		for(int rose: roses) {
+			if(rose > currentDay) {
+				roseCount = 0;
+			}else {
+				roseCount++;
+				if(roseCount == k) {
+					roseCount = 0;
+					bouquetCount++;
+				}
+			}
+		}
+		return bouquetCount >= n;
 	}
 }
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class TestCode {
 	public static void main(String[] args) {
-		DecreasingSubsequenceOA test = new DecreasingSubsequenceOA();
-		int[] nums = new int[] {1, 1, 1};
-		System.out.println(test.minNumSplit(nums));
+		MinDaysBloomOA test = new MinDaysBloomOA();
+		int[] roses = new int[] {1, 2, 4, 9, 3, 4, 1};
+		System.out.println(test.minDaysBloom(roses, 2, 2));
 	}
 }
