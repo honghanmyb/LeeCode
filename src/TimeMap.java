2f459a3614f4a10a48f32cb5a8d350f02cff7222
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2019-11-27 23:34:31 -0500

    Leecode 981 Time-based Key value store, use hashmap and treemap

--- src/TimeMap.java
+++ src/TimeMap.java
@@ -1,2 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TimeMap {
+    private Map<String, Element> timeMap;
+    public TimeMap() {
+        this.timeMap = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if(this.timeMap.containsKey(key)) {
+            Element element = this.timeMap.get(key);
+            element.set(timestamp, value);
+        }else{
+            Element element = new Element();
+            element.set(timestamp, value);
+            this.timeMap.put(key, element);
+        }
+    }
+
+    public String get(String key, int timestamp) {
+        if(this.timeMap.containsKey(key)){
+            return this.timeMap.get(key).get(timestamp);
+        }
+        return "";
+    }
+
+    private class Element{
+        public int largestTimestamp = 0;
+        public TreeMap<Integer, String> map;
+
+        public Element(){
+            this.map = new TreeMap<>();
+        }
+
+        public void set(int timestamp, String value){
+            this.map.put(timestamp, value);
+            this.largestTimestamp = timestamp > this.largestTimestamp? timestamp: this.largestTimestamp;
+        }
+
+        public String get(int timestamp){
+            if(timestamp > this.largestTimestamp){
+                return this.map.get(this.largestTimestamp);
+            }
+            Integer floorKey = this.map.floorKey(timestamp);
+            return floorKey == null? "": this.map.get(floorKey);
+        }
+    }
 }
