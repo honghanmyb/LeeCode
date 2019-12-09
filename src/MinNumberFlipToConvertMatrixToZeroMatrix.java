43f82cc1eeb420004c62421c0af0bbf2d76950e2
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2019-12-09 14:49:10 -0500

    Leecode 1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix, use dfs and try every possible combinatons

--- src/MinNumberFlipToConvertMatrixToZeroMatrix.java
+++ src/MinNumberFlipToConvertMatrixToZeroMatrix.java
@@ -2,23 +2,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MinNumberFlipToConvertMatrixToZeroMatrix {
-    int minCount = -1;
+    private int minCount = -1;
     public int minFlips(int[][] mat) {
-        Set<Integer> status = new HashSet<>();
+        boolean[][] hasFlip = new boolean[mat.length][mat[0].length];
         int initialStatus = getStatus(mat);
         if(initialStatus == 0){
             return 0;
         }
-        status.add(initialStatus);
         for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat[0].length; j++){
-                findMinFlip(mat, status, 0, i, j);
+                findMinFlip(mat, hasFlip, 0, i, j);
             }
         }
         return minCount;
     }
 
-    private void findMinFlip(int[][] mat, Set<Integer> status, int preCount, int row, int column){
+    private void findMinFlip(int[][] mat, boolean[][] hasFlip, int preCount, int row, int column){
         if(minCount != -1 && minCount <= preCount){
             return;
         }
@@ -29,15 +28,17 @@ public class MinNumberFlipToConvertMatrixToZeroMatrix {
             flip(mat, row, column);
             return;
         }
-        for(int j = column + 1; j < mat[0].length; j++){
-            findMinFlip(mat, status, preCount + 1, row, j);
-        }
-        for(int i = row + 1; i < mat.length; i++){
+        hasFlip[row][column] = true;
+        for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat[0].length; j++){
-                findMinFlip(mat, status, preCount + 1, i, j);
+                if(hasFlip[i][j]){
+                    continue;
+                }
+                findMinFlip(mat, hasFlip, preCount + 1, i, j);
             }
         }
         flip(mat, row, column);
+        hasFlip[row][column] = false;
     }
 
     private void flip(int[][] mat, int row, int column){
