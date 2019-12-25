548c004c083e78eb4ae9096dc11c894354aa6b35
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2019-12-25 17:27:15 -0500

    Leecode 1296. Divide Array in Sets of K Consecutive Numbers, use hashmap

--- src/DivideArrayInSetsOfKConsecutiveNumbers.java
+++ src/DivideArrayInSetsOfKConsecutiveNumbers.java
@@ -1,33 +1,44 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DivideArrayInSetsOfKConsecutiveNumbers {
     public boolean isPossibleDivide(int[] nums, int k) {
         if(nums.length % k != 0){
             return false;
         }
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                continue;
-            }
-            int curNum = nums[i];
-            int curCount = 1;
-            nums[i] = 0;
-            for(int j = i; j < nums.length; j++){
-                if(nums[j] == curNum + 1){
-                    curNum = nums[j];
-                    curCount++;
-                    nums[j] = 0;
-                    if(curCount == k){
-                        curCount = 0;
-                        break;
-                    }
+        int min = Integer.MAX_VALUE;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            putEntry(map, num, 1);
+            min = Math.min(min, num);
+        }
+        while(!map.isEmpty()){
+            System.out.println(min);
+            for(int i = 0; i < k; i++){
+                if(map.containsKey(min + i)){
+                    putEntry(map, min + i, -1);
+                }else{
+                    return false;
                 }
             }
-            if(curCount != 0){
-                return false;
+            min = Integer.MAX_VALUE;
+            for(int key : map.keySet()){
+                min = Math.min(min, key);
             }
         }
         return true;
+    }
+
+    private void putEntry(Map<Integer, Integer> map, int key, int change){
+        int value = 0;
+        if(map.containsKey(key)){
+            value = map.get(key);
+        }
+        value += change;
+        if(value > 0){
+            map.put(key, value);
+        }else{
+            map.remove(key);
+        }
     }
 }
