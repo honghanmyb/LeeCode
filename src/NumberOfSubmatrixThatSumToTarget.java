0fda24e13f353aec1b743b1855a4d91ed3bcccf8
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-01-17 16:06:29 -0500

    Leecode 1074. Number of Submatrices That Sum to Target, updated version of prefix sum, time complexity O(n^3)

--- src/NumberOfSubmatrixThatSumToTarget.java
+++ src/NumberOfSubmatrixThatSumToTarget.java
@@ -1,51 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberOfSubmatrixThatSumToTarget {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int[][] matrixSum = new int[matrix.length][matrix[0].length];
-        matrixSum[0][0] = matrix[0][0];
-        for(int j = 1; j < matrix[0].length; j++){
-            matrixSum[0][j] = matrix[0][j] + matrixSum[0][j - 1];
-        }
-        for(int i = 1; i < matrix.length; i++){
-            matrixSum[i][0] = matrix[i][0] + matrixSum[i - 1][0];
-        }
-
-        for(int i = 1; i < matrix.length; i++){
-            for(int j = 1; j < matrix[0].length; j++){
-                matrixSum[i][j] = matrixSum[i - 1][j] + matrixSum[i][j - 1] - matrixSum[i - 1][j - 1] + matrix[i][j];
+        int[][] columnSum = new int[matrix.length + 1][matrix[0].length];
+        for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                columnSum[i + 1][j] = columnSum[i][j] + matrix[i][j];
             }
         }
-        int submatrixCount = 0;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                int count = countSubmatrix(matrixSum, i, j, target);
-                submatrixCount += count;
-            }
-        }
-        return submatrixCount;
-    }
-
-    private int countSubmatrix(int[][] matrixSum, int xLimit, int yLimit, int target){
         int count = 0;
-        for(int i = 0; i <= xLimit; i++){
-            for(int j = 0; j <= yLimit; j++){
-                if(getSum(matrixSum, i, j, xLimit, yLimit) == target){
-                    ++count;
+        for (int startRow = 1; startRow < columnSum.length; startRow++) {
+            for (int endRow = startRow; endRow < columnSum.length; endRow++) {
+                Map<Integer, Integer> sumToOccurTimes = new HashMap<>();
+                int curSum = 0;
+                for (int j = 0; j < columnSum[0].length; j++) {
+                    curSum += columnSum[endRow][j] - columnSum[startRow - 1][j];
+                    if (curSum == target) {
+                        ++count;
+                    }
+                    if (sumToOccurTimes.containsKey(curSum - target)) {
+                        count += sumToOccurTimes.get(curSum - target);
+                    }
+                    if (sumToOccurTimes.containsKey(curSum)) {
+                        sumToOccurTimes.put(curSum, sumToOccurTimes.get(curSum) + 1);
+                    } else {
+                        sumToOccurTimes.put(curSum, 1);
+                    }
                 }
             }
         }
         return count;
-    }
-
-    private int getSum(int[][] matrixSum, int x1, int y1, int x2, int y2){
-        if(x1 + y1 == 0){
-            return matrixSum[x2][y2];
-        }
-        if(x1 == 0){
-            return matrixSum[x2][y2] - matrixSum[x2][y1 - 1];
-        }
-        if(y1 == 0){
-            return matrixSum[x2][y2] - matrixSum[x1 - 1][y2];
-        }
-        return matrixSum[x2][y2] - matrixSum[x2][y1 - 1] - matrixSum[x1 - 1][y2] + matrixSum[x1 - 1][y1 - 1];
     }
 }
