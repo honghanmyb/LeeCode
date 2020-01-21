6a1ff09da707972b9bf11bc2764b4bd4cd1a3584
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-01-21 15:21:55 -0500

    Leecode 723. Candy Crush, if a element should be crushed, mark it as negetive

--- src/CandyCrush.java
+++ src/CandyCrush.java
@@ -9,74 +9,49 @@ public class CandyCrush {
         //update board
         List<int[]> crushCells = new ArrayList<>();
         while(true){
-            checkHorizontally(board, crushCells);
-            checkVertically(board, crushCells);
-            if(crushCells.isEmpty()){
+            if(!shouldUpdate(board)){
                 break;
             }
-            updateBoard(board, crushCells);
+            updateBoard(board);
         }
         return board;
     }
 
-    private void checkHorizontally(int[][] board, List<int[]> crushCells){
+    private boolean shouldUpdate(int[][] board){
+        boolean update = false;
         for(int i = 0; i < board.length; i++){
-            boolean find = false;
             for(int j = 1; j < board[0].length - 1; j++){
                 if(board[i][j] == 0){
                     continue;
                 }
-                if(!find){
-                    if(board[i][j - 1] == board[i][j] && board[i][j] == board[i][j + 1]){
-                        find = true;
-                        crushCells.add(new int[]{i, j - 1});
-                        crushCells.add(new int[]{i, j});
-                        crushCells.add(new int[]{i, j + 1});
-                    }
-                }else{
-                    if(board[i][j] == board[i][j + 1]){
-                        crushCells.add(new int[]{i, j + 1});
-                    }else{
-                        find = false;
-                    }
+                int curVal = Math.abs(board[i][j]);
+                if(Math.abs(board[i][j - 1]) == curVal && Math.abs(board[i][j + 1]) == curVal){
+                    update = true;
+                    board[i][j - 1] = board[i][j] = board[i][j + 1] = -curVal;
                 }
             }
         }
-    }
 
-    private void checkVertically(int[][] board, List<int[]> crushCells){
         for(int j = 0; j < board[0].length; j++){
-            boolean find = false;
             for(int i = 1; i < board.length - 1; i++){
                 if(board[i][j] == 0){
                     continue;
                 }
-                if(!find){
-                    if(board[i - 1][j] == board[i][j] && board[i][j] == board[i + 1][j]){
-                        find = true;
-                        crushCells.add(new int[]{i - 1, j});
-                        crushCells.add(new int[]{i, j});
-                        crushCells.add(new int[]{i + 1, j});
-                    }
-                }else{
-                    if(board[i][j] == board[i + 1][j]){
-                        crushCells.add(new int[]{i + 1, j});
-                    }else{
-                        find = false;
-                    }
+                int curVal = Math.abs(board[i][j]);
+                if(Math.abs(board[i - 1][j]) == curVal && Math.abs(board[i + 1][j]) == curVal){
+                    update = true;
+                    board[i - 1][j] = board[i][j] = board[i + 1][j] = -curVal;
                 }
             }
         }
+        return update;
     }
 
-    private void updateBoard(int[][] board, List<int[]> crushCells){
-        for(int[] cell : crushCells){
-            board[cell[0]][cell[1]] = 0;
-        }
+    private void updateBoard(int[][] board){
         for(int j = 0; j < board[0].length; j++){
             int emptyCount = 0;
             for(int i = board.length - 1; i >= 0; i--){
-                if(board[i][j] == 0){
+                if(board[i][j] < 0){
                     emptyCount++;
                 }else{
                     board[i + emptyCount][j] = board[i][j];
@@ -86,6 +61,5 @@ public class CandyCrush {
                 board[i][j] = 0;
             }
         }
-        crushCells.clear();
     }
 }
