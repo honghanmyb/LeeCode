c01b7fc194f8c50d7ed3941364529c64b1dd31f9
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-01-22 16:13:46 -0500

    Leecode 833. Find And Replace in String, use a int array to record if string s at index i can be replaced, if so, then arr[i] record the index of the target string array, then use string builder to build the string, if arr[i] larger or equal than 0, meaning that the following string should be replaced, then repalce the string and increment the index, if not, append the char and increment the index by 1

--- src/FindAndReplaceInString.java
+++ src/FindAndReplaceInString.java
@@ -3,33 +3,23 @@ import java.util.Comparator;
 
 public class FindAndReplaceInString {
     public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
-        int offSet = 0;
-        Node[] nodes = new Node[indexes.length];
-        for(int i = 0; i < nodes.length; i++){
-            nodes[i] = new Node(indexes[i], sources[i], targets[i]);
-        }
-        Arrays.sort(nodes, Comparator.comparingInt(node -> node.index));
-        for(Node node : nodes){
-            int startIndex = node.index + offSet;
-            int endIndex = startIndex + node.source.length();
-            String curS = S.substring(startIndex, endIndex);
-            if(curS.equals(node.source)){
-                S = S.substring(0, startIndex) + node.target + S.substring(endIndex);
-                offSet += node.target.length() - node.source.length();
+        int[] replaceIndexes = new int[S.length()];
+        Arrays.fill(replaceIndexes, -1);
+        for(int i = 0; i < indexes.length; i++){
+            if(S.substring(indexes[i], indexes[i] + sources[i].length()).equals(sources[i])){
+                replaceIndexes[indexes[i]] = i;
             }
         }
-        return S;
-    }
-
-    private class Node{
-        int index;
-        String source;
-        String target;
-
-        Node(int index, String source, String target){
-            this.index = index;
-            this.source = source;
-            this.target = target;
+        StringBuilder newS = new StringBuilder("");
+        int index = 0;
+        while(index < S.length()){
+            if(replaceIndexes[index] < 0){
+                newS.append(S.charAt(index++));
+                continue;
+            }
+            newS.append(targets[replaceIndexes[index]]);
+            index += sources[replaceIndexes[index]].length();
         }
+        return newS.toString();
     }
 }
