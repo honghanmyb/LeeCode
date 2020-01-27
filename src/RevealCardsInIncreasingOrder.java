3af227a2128eb5a91f56b79c8bdb65ceb742bb31
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-01-27 12:52:36 -0500

    Leecode 950. Reveal Cards In Increasing Order, use queue to do a simulation, queue stores the index of decked sorted array, each time poll a num from queue, and cope it with a value in deck, then poll another index from queue and add it to the end

--- src/RevealCardsInIncreasingOrder.java
+++ src/RevealCardsInIncreasingOrder.java
@@ -1,49 +1,20 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RevealCardsInIncreasingOrder {
     public int[] deckRevealedIncreasing(int[] deck) {
-        List<Integer> unsorted = new ArrayList<>(deck.length);
-        for(int deckNum : deck){
-            unsorted.add(deckNum);
-        }
-        unsorted.sort(Comparator.comparingInt(anInt -> anInt));
-        List<Integer> deckSorted = deckSort(unsorted);
+        Arrays.sort(deck);
         int[] sorted = new int[deck.length];
-        for(int i = 0; i < sorted.length; i++){
-            sorted[i] = deckSorted.get(i);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < deck.length; i++) {
+            queue.add(i);
         }
+        for (int i = 0; i < deck.length - 1; i++) {
+            sorted[queue.poll()] = deck[i];
+            queue.add(queue.poll());
+        }
+        sorted[queue.poll()] = deck[deck.length - 1];
         return sorted;
-    }
-
-    private List<Integer> deckSort(List<Integer> unsorted){
-        int size = unsorted.size();
-        if(size <= 2){
-            return unsorted;
-        }
-        List<Integer> sorted = new ArrayList<>(size / 2);
-        List<Integer> newUnsorted = new ArrayList<>(size / 2);
-        int endIndex = (size - 1) / 2;
-        for(int i = 0; i <= endIndex; i ++){
-            sorted.add(unsorted.get(i));
-        }
-        for(int i = endIndex + 1; i < size; i++){
-            newUnsorted.add(unsorted.get(i));
-        }
-        List<Integer> newSorted = deckSort(newUnsorted);
-        if(size % 2 == 1){
-            newSorted.add(0, newSorted.get(newSorted.size() - 1));
-            newSorted.remove(newSorted.size() - 1);
-        }
-        unsorted.clear();
-        for(int i = 0; i < sorted.size(); i++){
-            unsorted.add(sorted.get(i));
-            if(i < newSorted.size()){
-                unsorted.add(newSorted.get(i));
-            }
-        }
-        return unsorted;
     }
 }
