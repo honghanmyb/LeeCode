3c33ed38ccb17fb4a4165e12d91ddfeb3c542097
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-01-28 21:55:34 -0500

    Leecode 844. Backspace String Compare, do the compare in place, check strings from back to front

--- src/BackspaceStringCompare.java
+++ src/BackspaceStringCompare.java
@@ -1,26 +1,37 @@
-import java.util.LinkedList;
-
 public class BackspaceStringCompare {
     public boolean backspaceCompare(String S, String T) {
-        return getString(S).equals(getString(T));
+        int sPos = S.length() - 1;
+        int tPos = T.length() - 1;
+        while (true) {
+            sPos = findNextValidPos(S, sPos);
+            tPos = findNextValidPos(T, tPos);
+            if (sPos < 0 && tPos < 0) {
+                break;
+            } else if (sPos < 0 || tPos < 0) {
+                return false;
+            }
+            if (S.charAt(sPos--) != T.charAt(tPos--)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    private String getString(String s){
-        LinkedList<Character> list = new LinkedList<>();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch != '#'){
-                list.addLast(ch);
-            }else{
-                if(!list.isEmpty()){
-                    list.pollLast();
+    private int findNextValidPos(String s, int pos) {
+        int backspaceCount = 0;
+        while (pos >= 0) {
+            if (s.charAt(pos) == '#') {
+                backspaceCount++;
+                --pos;
+            } else {
+                if (backspaceCount == 0) {
+                    return pos;
+                } else {
+                    --backspaceCount;
+                    --pos;
                 }
             }
         }
-        StringBuilder current = new StringBuilder();
-        for(char ch : list){
-            current.append(ch);
-        }
-        return current.toString();
+        return -1;
     }
 }
