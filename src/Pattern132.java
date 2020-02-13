0d690e05f818ce1d0694f81dc945f633cb8f7fa9
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-02-13 15:06:24 -0500

    Leecode 456. 132 Pattern, use treeset, maintain two arrays, one record the min num from start to the current element, one record the max num from current element to end that smaller than current element, time complexity O(nlogn)

--- src/Pattern132.java
+++ src/Pattern132.java
@@ -1,13 +1,29 @@
+import java.util.TreeSet;
+
 public class Pattern132 {
     public boolean find132pattern(int[] nums) {
-        for(int i = 0; i < nums.length - 2; i++){
-            int curMin = nums[i];
-            int curMax = curMin + 1;
-            for(int j = i + 1; j < nums.length; j++){
-                curMax = Math.max(curMax, nums[j]);
-                if(nums[j] > curMin && nums[j] < curMax){
-                    return true;
-                }
+        if(nums.length < 3){
+            return false;
+        }
+        int[] minLeft = new int[nums.length];
+        int[] maxRight = new int[nums.length];
+        int min = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            min = Math.min(min, nums[i]);
+            minLeft[i] = min;
+        }
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i = nums.length - 1; i >= 0; i--){
+            Integer maxLower = set.lower(nums[i]);
+            maxRight[i] = Integer.MAX_VALUE;
+            if(maxLower != null){
+                maxRight[i] = maxLower;
+            }
+            set.add(nums[i]);
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(minLeft[i] < nums[i] && nums[i] > maxRight[i] && maxRight[i] > minLeft[i]){
+                return true;
             }
         }
         return false;
