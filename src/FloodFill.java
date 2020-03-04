4e2133da95d6397fbe4d2036991d2b8bf03bf516
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-03-04 15:24:04 -0500

    Leecode 733. Flood Fill, use dfs

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -2,28 +2,48 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class FloodFill {
+    //    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+//        int oldColor = image[sr][sc];
+//        if(oldColor == newColor){
+//            return image;
+//        }
+//        Queue<int[]> queue = new LinkedList<>();
+//        int[][] directions = new int[][]{
+//                {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+//        };
+//        queue.add(new int[]{sr, sc});
+//        while(!queue.isEmpty()){
+//            int[] curCell = queue.poll();
+//            image[curCell[0]][curCell[1]] = newColor;
+//            for(int[] direction : directions){
+//                int newRow = curCell[0] + direction[0];
+//                int newCol = curCell[1] + direction[1];
+//                if(newRow < 0 || newRow >= image.length || newCol < 0 || newCol >= image[0].length) continue;
+//                if(image[newRow][newCol] == oldColor){
+//                    queue.add(new int[]{newRow, newCol});
+//                }
+//            }
+//        }
+//        return image;
+//    }
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int oldColor = image[sr][sc];
-        if(oldColor == newColor){
+        if (oldColor == newColor) {
             return image;
         }
-        Queue<int[]> queue = new LinkedList<>();
         int[][] directions = new int[][]{
                 {0, 1}, {0, -1}, {1, 0}, {-1, 0}
         };
-        queue.add(new int[]{sr, sc});
-        while(!queue.isEmpty()){
-            int[] curCell = queue.poll();
-            image[curCell[0]][curCell[1]] = newColor;
-            for(int[] direction : directions){
-                int newRow = curCell[0] + direction[0];
-                int newCol = curCell[1] + direction[1];
-                if(newRow < 0 || newRow >= image.length || newCol < 0 || newCol >= image[0].length) continue;
-                if(image[newRow][newCol] == oldColor){
-                    queue.add(new int[]{newRow, newCol});
-                }
-            }
-        }
+        dfs(image, sr, sc, oldColor, newColor, directions);
         return image;
+    }
+
+    private void dfs(int[][] image, int row, int col, int oldColor, int newColor, int[][] directions) {
+        if (row < 0 || row >= image.length || col < 0 || col >= image[0].length) return;
+        if (image[row][col] != oldColor) return;
+        image[row][col] = newColor;
+        for (int[] direction : directions) {
+            dfs(image, row + direction[0], col + direction[1], oldColor, newColor, directions);
+        }
     }
 }
